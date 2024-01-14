9b8b0df9d790819b0382a2ae2d0a00c45f1f0b37
Dominik <55330408+Dom-in@users.noreply.github.com>
2024-01-14 11:00:45 +0000

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,34 +3,46 @@ fx_version 'cerulean'
 use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 game 'gta5'
+
 -- Resource Information
 name 'ox_target'
 author 'Overextended'
-version '1.6.3'
+version '1.13.1'
 repository 'https://github.com/overextended/ox_target'
 description ''
+
 -- Manifest
 ui_page 'web/index.html'
+
 shared_scripts {
 	'@ox_lib/init.lua',
 }
+
 client_scripts {
 	'@ox_lib/init.lua',
-	'client/utils.lua',
-	'client/api.lua',
-	'client/debug.lua',
-	'client/defaults.lua',
-	'client/framework/*.lua',
-	'client/compat/*.lua',
 	'client/main.lua',
 }
+
 server_scripts {
 	'server/main.lua'
 }
+
 files {
 	'web/**',
-	'locales/*.json'
+	'locales/*.json',
+	'client/api.lua',
+	'client/utils.lua',
+	'client/state.lua',
+	'client/debug.lua',
+	'client/defaults.lua',
+	'client/framework/ox.lua',
+	'client/framework/esx.lua',
+	'client/framework/qb.lua',
+	'client/compat/qtarget.lua',
+	'client/compat/qb-target.lua',
 }
-provide 'ox_target'
+
+provide 'qtarget'
 provide 'qb-target'
+
 dependency 'ox_lib'
